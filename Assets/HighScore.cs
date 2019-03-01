8494ed6065f58ad513171cfa4ffeb47e3abc7693
Sarahsfanous <47231253+Sarahsfanous@users.noreply.github.com>
2019-02-28 22:16:30 -0500

    File Build

---
+++ Assets/HighScore.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+//Remember, we need this line for uGUI to work. 
+
+public class HighScore : MonoBehaviour
+{
+    static public int score = 1000;
+
+    void Awake()
+    {
+        //a
+        //If the PlayerPrefs HighScore already exists, read it 
+        if (PlayerPrefs.HasKey("HighScore"))
+        {
+            //b
+            score = PlayerPrefs.GetInt("HighScore");
+        }
+        //Assign the high score to HighScore
+        PlayerPrefs.SetInt("HighScore", score);
+        //c
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Text gt = this.GetComponent<Text>();
+        gt.text = "High Score: " + score;
+        //Update the PlayerPrefs HighScore if necessary
+        if (HighScore.score > PlayerPrefs.GetInt("HighScore"))
+        {
+            PlayerPrefs.SetInt("HighScore", HighScore.score);
+        }
+    }
+}
